01a4b75943e3f68de9e46b9d1c5ed873ef55b7c5
Anton Sustavov <asustavov@unitedthinkers.com>
2019-01-12 19:19:51 +0200

    some commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Hi! " + i);
