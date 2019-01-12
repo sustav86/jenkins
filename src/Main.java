ac884f6395257221a7d9021c5c29503a17e61621
Anton Sustavov <asustavov@unitedthinkers.com>
2019-01-12 19:16:50 +0200

    add i to loop

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Hello World!");
 
         for (int i = 0; i < 10; i++) {
-            System.out.println("Hi!");
+            System.out.println("Hi! " + i);
         }
     }
 }
